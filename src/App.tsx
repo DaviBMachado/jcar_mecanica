52470e93c5dc0a2fa61294106ac24b895803fd81
Matheus-Salinas <matheussalinonhas@gmail.com>
2026-05-09 21:51:12 -0300

    Feat: Subindo atualização automatica para subir para topo da tela ao trocar de pagina

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import { RootLayout } from './layouts/RootLayout';
 import { Home } from './pages/Home';
 import { Services } from './pages/Services';
 import { About } from './pages/About';
+import { ScrollToTop } from './components/ScrollToTop'; 
 
 function App() {
   return (
     <BrowserRouter>
+      {/* 2. O componente invisível fica aqui, vigiando as trocas de página */}
+      <ScrollToTop />
+      
       <Routes>
         <Route path="/" element={<RootLayout />}>
           <Route index element={<Home />} />
